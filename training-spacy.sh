4d0d4f140c55ea9d033c8940e150da1cecee4996
nishkalavallabhi <vbsowmya@gmail.com>
2021-02-17 11:06:49 -0500

    shell scripts for spacy

---
+++ training-spacy.sh
@@ -0,0 +1,5 @@
+# training NER models with spacy CLI, for the 4 genres of text in OntoNotes
+python3 -m spacy train en train-news spacy-format/train/onto.news.ner.json spacy-format/development/onto.news.ner.json -G -p ner
+python3 -m spacy train en train-bc spacy-format/train/onto.bc.ner.json spacy-format/development/onto.bc.ner.json -G -p ner
+python3 -m spacy train en train-tc spacy-format/train/onto.news.tc.json spacy-format/development/onto.tc.ner.json -G -p ner
+python3 -m spacy train en train-wb  spacy-format/train/onto.news.wb.json spacy-format/development/onto.wb.ner.json -G -p ner
