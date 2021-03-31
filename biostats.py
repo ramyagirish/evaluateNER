c981555ec49c4c799e942d8d507bff068dd7b63c
nishkalavallabhi <vbsowmya@gmail.com>
2021-03-31 09:35:51 -0400

    corrected a bug in stats calculation

--- biostats.py
+++ biostats.py
@@ -3,13 +3,14 @@ dir = "C:/Users/vajjalas/Downloads/NERProject_Materials/bio/bio/"
 subdirs = ["train", "test", "development"]
 filepaths = ["onto.bc.ner", "onto.bn.ner", "onto.mz.ner", "onto.nw.ner", "onto.tc.ner", "onto.wb.ner"]
 
-mydict = {} #keys are <NE,tag> tuples, values are their counts.
 for subdir in subdirs:
     for afile in filepaths:
         fh = open(os.path.join(dir, subdir, afile), encoding="utf-8")
+        mydict = {}  # keys are <NE,tag> tuples, values are their counts.
         fh.readline()
         tempsen = []
         tempnet = []
+        numsens = 0
         for line in fh:
             if line.strip() is not "":
               splits = line.strip().split("\t")
@@ -32,6 +33,7 @@ for subdir in subdirs:
                     tempsen = [mystr]
                     tempnet = [tag]
             else:
+                numsens += 1
                 if tempsen:
                     mytuple = (" ".join(tempsen), tempnet[0])
                     mydict[mytuple] = mydict.get(mytuple, 0) + 1
@@ -39,9 +41,10 @@ for subdir in subdirs:
                     tempnet = []
 
         fh.close()
-
+        total_ents = len(mydict.keys())
         sorteddict = sorted(mydict.items(), key=lambda x: x[1], reverse=True)
         output_path = open(subdir+"-"+afile+"-dist.txt", "w", encoding="utf-8")
+        print("Total entities and sentences in ", subdir+ "-" + afile +":  ", str(total_ents))
         for key,val in sorteddict:
             output_path.write(str(key) + "\t"+str(val))
             output_path.write("\n")
