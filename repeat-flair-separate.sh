d2d1326dfe2cb777ce6cf4ac32962953c7aee0c5
nishkalavallabhi <vbsowmya@gmail.com>
2021-04-21 21:04:45 -0400

    i forgot git rm

--- repeat-flair-separate.sh
+++
@@ -1,4 +0,0 @@
-python3 evaluation.py bio-combined/test/onto.news.ner > outputs/onto_flair_combined.news.ner.txt
-python3 evaluation.py bio-combined/test/onto.bc.ner > outputs/onto_flair_combined.bc.ner.txt
-python3 evaluation.py bio-combined/test/onto.tc.ner > outputs/onto_flair_combined.tc.ner.txt
-python3 evaluation.py bio-combined/test/onto.wb.ner > outputs/onto_flair_combined.wb.ner.txt
